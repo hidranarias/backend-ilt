dc474d23d7119ad1b5743be604f4fe6525197020
Hidran <hidran.arias@spryker.com>
2022-11-07 22:17:48 +0100

    Extending Spryker functionality - Part 3. Adds trainingclient and antelopetransformer to customerpage

--- src/Pyz/Yves/CustomerPage/CustomerPageDependencyProvider.php
+++ src/Pyz/Yves/CustomerPage/CustomerPageDependencyProvider.php
@@ -7,12 +7,21 @@
 
 namespace Pyz\Yves\CustomerPage;
 
+use Spryker\Yves\Kernel\Container;
 use SprykerShop\Yves\AgentPage\Plugin\FixAgentTokenAfterCustomerAuthenticationSuccessPlugin;
 use SprykerShop\Yves\CustomerPage\CustomerPageDependencyProvider as SprykerShopCustomerPageDependencyProvider;
 use SprykerShop\Yves\CustomerReorderWidget\Plugin\CustomerPage\CustomerReorderWidgetPlugin;
 
 class CustomerPageDependencyProvider extends SprykerShopCustomerPageDependencyProvider
 {
+    public const CLIENT_TRAINING = 'CLIENT_TRAINING';
+
+    public function provideDependencies(Container $container): Container
+    {
+        $container = parent::provideDependencies($container);
+        return $this->addTrainingClient($container);
+    }
+
     /**
      * @return string[]
      */
@@ -51,5 +60,14 @@ class CustomerPageDependencyProvider extends SprykerShopCustomerPageDependencyPr
         return [
             new FixAgentTokenAfterCustomerAuthenticationSuccessPlugin(),
         ];
+    }
+
+    protected function addTrainingClient(Container $container)
+    {
+        $container->set(static::CLIENT_TRAINING, function (Container $container) {
+            return $container->getLocator()->training()->client();
+        });
+
+        return $container;
     }
 }
